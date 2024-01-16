9b9f4f998eaaea53467bd6f484b75d5abe75b195
shahid <shahid72591@gmail.com>
2024-01-16 12:27:42 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { CreateTodo } from './components/CreateTodo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <CreateTodo />
+   </div>
+  )
+}
+
+export default App
