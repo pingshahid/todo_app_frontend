03366231bac4bd89a1ada62ef846c206f0c04871
shahid <shahid72591@gmail.com>
2024-01-16 12:46:53 +0530

    Todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { CreateTodo } from './components/CreateTodo'
+import { Todos } from './components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
    <div>
     <CreateTodo />
+    <Todos></Todos>
    </div>
   )
 }
---
+++ src/components/Todos.jsx
@@ -0,0 +1,7 @@
+export function Todos{
+    return <div>
+        <h1>Go to Gym</h1>
+        <h1>Go to Gym</h1>
+        <button>Mark as completed</button>
+    </div>
+}
